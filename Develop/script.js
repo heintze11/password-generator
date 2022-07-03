283b8ef4d1ad01f2d86dac6c49e66da87539b3e1
Levi H <levi.heintzelman@gmail.com>
2022-07-03 15:09:46 -0600

    Pseudocode and prompt/confirm boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,29 @@
+//Get button to pop up questions about criteria using prompts
+//    Prompt and Confirm
+//length 8-128 - check if valid
+//lowercase, uppercase, numeric, special characters 
+//Need at least 1 type of criteria to create password - Check if valid
+//Run random generator
+//post random password - appears to be done
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// const lowercase = [];
+// const uppercase = [];
+// const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const specialChar = [`~`, `!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`, `_`, `-`, `+`, `=`, `{`, `[`, `}`, `]`, `|`, `\`, `:`, `;`, `<`, `,`, `>`, `.`, `?`, `/` ];
+
+//create generatePassword Function
+function generatePassword() {
+  var length = prompt ("How many characters would you like your password to contain?");
+  var uppercase = confirm ("Would you like to use Uppercase?")
+  var lowercase = confirm ("Would you like to use Lowercase?")
+  var numeric = confirm ("Would you like to use numbers?")
+  var specialChar = confirm ("Would you like to use Special Characters")
+
+
+  return ("Generate Password will go here")
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +36,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
