0e591b7b41cd71464500964767a31be6e2a6070f
Levi H <levi.heintzelman@gmail.com>
2022-07-09 11:49:25 -0600

    Clean up code and file structure

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,9 @@
 //Get button to pop up questions about criteria using prompts
-//    Using Prompt and Confirm
-//length 8-128 - check if valid
-//lowercase, uppercase, numeric, special characters 
-//Need at least 1 type of criteria to create password - Check if valid
-//Run random generator
-  // use for loop to pull random variables length number of times
-//post random password - *appears to be mostly done
+
 
 // Assignment Code
+// establish variables
+//lowercase, uppercase, numeric, special characters 
 let generateBtn = document.querySelector("#generate");
 const uc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -19,9 +15,10 @@ let password = [];
 //create generatePassword Function
 function generatePassword() {
   //Prompt for input values
-  var length = prompt ("How many characters would you like your password to contain?");
+  let length = prompt ("How many characters would you like your password to contain?");
    
-  //Do While loop! for length
+  //length 8-128 - check if valid
+  //Do While loop for length
   if (length < 8 || length > 128) {
     do {
      length = prompt ("Please choose a number between 8 and 128.")
@@ -30,17 +27,18 @@ function generatePassword() {
   }
 
   // Confirm for character types
-  var uppercase = confirm ("Would you like to use Uppercase?");
-  var lowercase = confirm ("Would you like to use Lowercase?");
-  var numeric = confirm ("Would you like to use Numbers?");
-  var specialChar = confirm ("Would you like to use Special Characters");
+  let uppercase = confirm ("Would you like to use Uppercase?");
+  let lowercase = confirm ("Would you like to use Lowercase?");
+  let numeric = confirm ("Would you like to use Numbers?");
+  let specialChar = confirm ("Would you like to use Special Characters");
 
-
-  // create if statements to push array to new array if true
+  //Need at least 1 type of criteria to create password - Check if valid
+  // If all are null, please try again
   if (!uppercase && !lowercase && !numeric && !specialChar) {
     alert ("Please select at least one type of character. Please try again.")
     return ("Please select one character type")
   } 
+  // create if statements to push array to new array if true
   if (uppercase){
      passwordArray=passwordArray.concat(uc) 
     } 
@@ -55,12 +53,11 @@ function generatePassword() {
   }
  
   // use for loop to pull random variables "length" number of times
+  //post random password to password
   for (let index = 0; index < length; index++) {
-   // console.log(passwordArray[Math.floor(Math.random(passwordArray.length))])
     password.push(passwordArray[Math.floor(Math.random()*passwordArray.length)])
   }
-  console.log(password)
-
+ 
   passwordArray = [];
   return (password)
 }
@@ -71,6 +68,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = passwordDisplay;
+  // clear password for next use
   password = [];
 }
 
